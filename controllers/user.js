52c3887f0ce4118ad0abdad00aa2292d8495fdf4
vicanso <vicansocanbico@gmail.com>
2015-03-25 21:45:13 +0800

    添加user++ uv++的处理

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,33 @@
 'use strict';
+var uuid = require('node-uuid');
+var moment = require('moment');
 module.exports = function *(){
   var method = this.method;
-  this.body = {
-    name : 'vicanso'
-  };
+
+  switch(method){
+    case 'GET':
+      getUserSession(this);
+    break;
+  }
 };
+
+
+function getUserSession(ctx){
+  var cookies = ctx.cookies;
+  if(!cookies.get('jtuuid')){
+    cookies.set('jtuuid', uuid.v4(), {
+      maxAge : 365 * 24 * 3600 * 1000
+    });
+    // TODO 记录为用户第一次打开（新增用户）
+    console.info('user++');
+  }
+  if(!cookies.get('vicanso')){
+    // TODO 记录UV
+    console.info('uv++');
+  }
+  ctx.body = {
+    name : 'vicanso',
+    now : Date.now(),
+    code : uuid.v4()
+  };
+}
